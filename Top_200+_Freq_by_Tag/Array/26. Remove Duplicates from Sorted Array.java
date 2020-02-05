3f72fa20d93589d298cd31ba5bdab2b77ed1ccc0
desperatecat <376198935@qq.com>
2020-02-04 23:20:35 -0500

    update 26

--- Top_200+_Freq_by_Tag/Array/26. Remove Duplicates from Sorted Array.java
+++ Top_200+_Freq_by_Tag/Array/26. Remove Duplicates from Sorted Array.java
@@ -17,3 +17,19 @@ class Solution {
         return i+1;     //when j reach the end of array, i+1 is the length of the new non-duplicate array
     }
 }
+
+
+
+//more clear two poiters solution
+
+public int removeDuplicates(int[] nums) {
+    if (nums.length == 0) return 0;
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+        if (nums[j] != nums[i]) {
+            i++;
+            nums[i] = nums[j];
+        }
+    }
+    return i + 1;
+}
