2c1d03cca4c54f542af2d23114bf73cee4670d1d
desperatecat <376198935@qq.com>
2020-02-05 22:13:06 -0500

    update 35

--- Top_200+_Freq_by_Tag/Array/35. Search Insert Position.java
+++ Top_200+_Freq_by_Tag/Array/35. Search Insert Position.java
@@ -10,3 +10,21 @@ class Solution {
         return nums.length; //if target is bigger than the last element, then put target in the end of array, whose index is nums.length
     }
 }
+
+
+
+
+//binary search
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0, high = nums.length-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] > target) high = mid-1;
+            else low = mid+1;
+        }
+        return low;
+    }
+}
