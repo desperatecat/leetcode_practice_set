cf6880348b4be19e9c72767e2316f1e1a28115b7
desperatecat <376198935@qq.com>
2020-02-08 15:25:19 -0500

    217

--- Top_200+_Freq_by_Tag/Array/189. Rotate Array.java
+++ Top_200+_Freq_by_Tag/Array/189. Rotate Array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         
-        k%=nums.length;
+        k%=nums.length;//in case k>nums.length
         
         reverse(nums, 0, nums.length-1);
         reverse(nums, 0, k-1);
