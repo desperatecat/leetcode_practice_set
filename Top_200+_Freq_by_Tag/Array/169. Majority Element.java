2c3defb07baed2f7ec46f07177a810ce7abe498c
desperatecat <376198935@qq.com>
2020-02-08 14:44:13 -0500

    hashmap

--- Top_200+_Freq_by_Tag/Array/169. Majority Element.java
+++ Top_200+_Freq_by_Tag/Array/169. Majority Element.java
@@ -7,7 +7,20 @@ class Solution {
 
 
 
-
+// HashMap
+public int majorityElement(int[] nums) {
+    Map<Integer, Integer> map = new HashMap<>();
+    int maxNum = 0, maxCount = 0;
+    for (int num: nums) {
+      int count = map.getOrDefault(num, 0) + 1;
+      map.put(num, count);
+      if (count > maxCount) {
+        maxCount = count;
+        maxNum = num;
+      }
+    }
+    return maxNum;
+  }
 
 
 
