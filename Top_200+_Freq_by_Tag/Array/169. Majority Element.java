b5b18e0990ea4be8fb4eb34738994fc8b4ea335d
desperatecat <376198935@qq.com>
2020-02-08 14:47:10 -0500

    Boyer-Moore Voting Algorithm

--- Top_200+_Freq_by_Tag/Array/169. Majority Element.java
+++ Top_200+_Freq_by_Tag/Array/169. Majority Element.java
@@ -26,13 +26,30 @@ public int majorityElement(int[] nums) {
 
 
 //Boyer-Moore Voting Algorithm
-    public int majorityElement(int[] nums){
-        int count = 0;
-        Integer candidate = null;
-        
-        for (int num:nums){
-            if(count==0) candidate=num;
-            count += (num==candidate)? 1 : -1;
-        }
-        return candidate;
+public int majorityElement(int[] nums){
+    int count = 0;
+    Integer candidate = null;
+    
+    for (int num:nums){
+        if(count==0) candidate=num;
+        count += (num==candidate)? 1 : -1;
     }
+    return candidate;
+}
+
+//Boyer-Moore Voting Algorithm
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC169%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E6%B1%82%E4%BC%97%E6%95%B0.md
+public int majorityElement(int[] nums) {
+    int candidate = nums[0], count = 1;
+    for (int i = 1; i < nums.length; ++i) {
+      if (count == 0) {
+        candidate = nums[i];
+        count = 1;
+      } else if (nums[i] == candidate) {
+        count++;
+      } else{
+        count--;
+      }
+    }
+    return candidate;
+  }
