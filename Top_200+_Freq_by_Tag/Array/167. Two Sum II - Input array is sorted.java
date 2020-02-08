18bf211a7d91df08e01addc60bf5fc278874a1b9
desperatecat <376198935@qq.com>
2020-02-07 22:32:03 -0500

    update

--- Top_200+_Freq_by_Tag/Array/167. Two Sum II - Input array is sorted.java
+++ Top_200+_Freq_by_Tag/Array/167. Two Sum II - Input array is sorted.java
@@ -17,3 +17,26 @@ class Solution {
         throw new IllegalArgumentException("No two sum result!");
     }
 }
+
+
+
+
+//two pointers:https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC167%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E4%B8%A4%E6%95%B0%E4%B9%8B%E5%92%8CII-%E8%BE%93%E5%85%A5%E6%9C%89%E5%BA%8F%E6%95%B0%E7%BB%84.md
+public int[] twoSum(int[] num, int target) {
+    int[] indice = new int[2];
+    if (num == null || num.length < 2) return indice;
+    int left = 0, right = num.length - 1;
+    while (left < right) {
+        int v = num[left] + num[right];
+        if (v == target) {
+            indice[0] = left + 1;
+            indice[1] = right + 1;
+            break;
+        } else if (v > target) {
+            right --;
+        } else {
+            left ++;
+        }
+    }
+    return indice;
+}
