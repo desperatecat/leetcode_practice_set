9f1073e33ad258ba2a6ec142e45be2def3725a22
desperatecat <376198935@qq.com>
2020-02-09 16:29:20 -0500

    update 219

--- Top_200+_Freq_by_Tag/Array/219. Contains Duplicate II.java
+++ Top_200+_Freq_by_Tag/Array/219. Contains Duplicate II.java
@@ -16,3 +16,19 @@ class Solution {
         else return false;
     }
 }
+
+
+
+
+//slide window
+//https://blog.csdn.net/fuxuemingzhu/article/details/77387169
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i - k - 1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
