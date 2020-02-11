8b7866a10c66ecc6bbd326d0d7466a4b6a1948cb
desperatecat <376198935@qq.com>
2020-02-10 22:52:24 -0500

    746

---
+++ Top_200+_Freq_by_Tag/Array/746. Min Cost Climbing Stairs.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int f1=0, f2=0;
+
+        for(int i = cost.length-1; i>=0; i--){
+            int f0 = cost[i] + Math.min(f1, f2);
+            f2=f1;
+            f1=f0;
+        }
+
+        return Math.min(f1, f2);
+    }
+}
+
+
+
+//greedy and dp
+public int minCostClimbingStairs(int[] cost) {
+        for (int i = 2; i < cost.length; i++) {
+            cost[i] += Math.min(cost[i-1], cost[i-2]);
+        }
+        return Math.min(cost[cost.length-1], cost[cost.length-2]);
+    }
