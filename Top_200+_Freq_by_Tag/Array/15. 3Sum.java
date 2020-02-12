2e6493aa9bfa6c0ce1baec1b361997ee2b12e89c
desperatecat <376198935@qq.com>
2020-02-11 19:10:34 -0500

    update

--- Top_200+_Freq_by_Tag/Array/15. 3Sum.java
+++ Top_200+_Freq_by_Tag/Array/15. 3Sum.java
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/3sum/discuss/7399/Easiest-Java-Solution
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
@@ -7,7 +8,7 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && (nums[i] == nums[i - 1]))
                 continue; // skip duplicate
-            int j = i + 1, k = nums.length - 1; // two pointers
+            int j = i + 1, k = nums.length - 1; // two pointers, j is start at i+1 since j counld not be the same as i
             int target = -nums[i]; // nums[i]+nums[j]+nums[k]=0, means: nums[j]+nums[k]=-nums[i]
             while (j < k) {
                 if (nums[j] + nums[k] == target) {
@@ -18,10 +19,10 @@ class Solution {
                         j++; // skip duplicate
                     while (j < k && nums[k] == nums[k + 1])
                         k--; // skip duplicate
-                } else if (nums[j] + nums[k] > target) {
-                    k--;
-                } else {
+                } else if (nums[j] + nums[k] < target) { // sorted array!!!
                     j++;
+                } else {
+                    k--;
                 }
             }
         }
