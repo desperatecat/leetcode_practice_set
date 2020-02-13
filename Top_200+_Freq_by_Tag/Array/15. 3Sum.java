9465859cc40fe7d083a0645dd870541283914f2b
desperatecat <376198935@qq.com>
2020-02-12 22:25:13 -0500

    update 15

--- Top_200+_Freq_by_Tag/Array/15. 3Sum.java
+++ Top_200+_Freq_by_Tag/Array/15. 3Sum.java
@@ -8,21 +8,21 @@ class Solution {
         for (int i = 0; i < nums.length - 2; i++) {
             if (i > 0 && (nums[i] == nums[i - 1])) // skip duplicate
                 continue;
-            int j = i + 1, k = nums.length - 1; // two pointers, j is start at i+1 since j counld not be the same as i
-            int target = -nums[i]; // nums[i]+nums[j]+nums[k]=0, means: nums[j]+nums[k]=-nums[i]
-            while (j < k) {
-                if (nums[j] + nums[k] == target) {
-                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                    while (j < k && nums[j] == nums[j - 1]) // skip duplicate
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1]) // skip duplicate
-                        k--;
-                } else if (nums[j] + nums[k] < target) { // sorted array!!!
-                    j++;
+            int left = i + 1, right = nums.length - 1; // two pointers, left is start at i+1 since left counld not be the same as i
+            int target = -nums[i]; // nums[i]+nums[left]+nums[right]=0, means: nums[left]+nums[right]=-nums[i]
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    list.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while (left < right && nums[left] == nums[left - 1]) // skip duplicate
+                        left++;
+                    while (left < right && nums[right] == nums[right + 1]) // skip duplicate
+                        right--;
+                } else if (nums[left] + nums[right] < target) { // sorted array!!!
+                    left++;
                 } else {
-                    k--;
+                    right--;
                 }
             }
         }
