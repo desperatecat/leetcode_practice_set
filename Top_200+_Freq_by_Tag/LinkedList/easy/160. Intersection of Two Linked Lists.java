e7a30ac289a10d8ccbcc63b16505e778099184ca
desperatecat <376198935@qq.com>
2020-02-17 12:34:04 -0500

    160 s2

--- Top_200+_Freq_by_Tag/LinkedList/easy/160. Intersection of Two Linked Lists.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/160. Intersection of Two Linked Lists.java
@@ -47,6 +47,18 @@ public class Solution {
         } 
         return cnt;
     }
+}
 
 
+//solution 2 (ignore length)
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA, b = headB;
+        while (a != b) {
+            a = (a != null) ? a.next : headB;
+            b = (b != null) ? b.next : headA;
+        }
+        return a;
+    }
 }
