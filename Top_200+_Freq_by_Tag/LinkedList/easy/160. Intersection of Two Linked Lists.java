656037993713ad61527d84a64e335d991c1cd8e8
desperatecat <376198935@qq.com>
2020-02-17 12:24:35 -0500

    160 s1

--- Top_200+_Freq_by_Tag/LinkedList/easy/160. Intersection of Two Linked Lists.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/160. Intersection of Two Linked Lists.java
@@ -1,3 +1,6 @@
+//https://github.com/grandyang/leetcode/issues/160
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -9,8 +12,41 @@
  *     }
  * }
  */
+
+
+ //solution 1 (normal)
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        
+        if(headA == null || headB == null){
+            return null;
+        }
+
+        int lenA = getLength(headA), lenB = getLength(headB);
+        if(lenA > lenB){
+            for (int i=0; i< lenA - lenB; i++){
+                headA = headA.next;
+            }
+        } else {
+            for (int i=0; i< lenB - lenA; i++){
+                headB = headB.next;
+            }
+        }
+
+        while(headA != null && headB != null && headA != headB){
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return (headA != null && headB != null) ? headA : null;
+    } 
+
+    public int getLength(ListNode head){
+        int cnt = 0;
+        while(head != null){
+            cnt++;
+            head = head.next;
+        } 
+        return cnt;
     }
+
+
 }
