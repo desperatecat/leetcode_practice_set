752b751e70a91b3e6b205a18542d8270990e961a
desperatecat <376198935@qq.com>
2020-02-16 21:59:31 -0500

    141 two pointers

--- Top_200+_Freq_by_Tag/LinkedList/easy/141. Linked List Cycle.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/141. Linked List Cycle.java
@@ -9,8 +9,22 @@
  *     }
  * }
  */
+
+
+
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC141%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E7%8E%AF%E5%BD%A2%E9%93%BE%E8%A1%A8.md
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        
+        ListNode fast = head, slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow) return true;
+
+        }
+        return false;
     }
 }
