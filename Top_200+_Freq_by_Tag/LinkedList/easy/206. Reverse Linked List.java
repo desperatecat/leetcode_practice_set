e8e993eedfd3f06f527836c789bc2069e6997a28
desperatecat <376198935@qq.com>
2020-02-17 15:58:49 -0500

    s2

--- Top_200+_Freq_by_Tag/LinkedList/easy/206. Reverse Linked List.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/206. Reverse Linked List.java
@@ -20,3 +20,17 @@ class Solution {
         return prev;
     }
 }
+
+
+
+//recursion
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
