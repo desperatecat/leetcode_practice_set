f7558e3e5bd00731837d59a3e12e5c1692cb882a
desperatecat <376198935@qq.com>
2020-02-17 15:40:08 -0500

    203 s c

--- Top_200+_Freq_by_Tag/LinkedList/easy/203. Remove Linked List Elements.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/203. Remove Linked List Elements.java
@@ -1,3 +1,5 @@
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC203%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E7%A7%BB%E9%99%A4%E9%93%BE%E8%A1%A8%E5%85%83%E7%B4%A0.md
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,6 +10,20 @@
  */
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
-        
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+        ListNode curr = dummyHead;
+
+        while(curr.next != null){
+            if(curr.next.val == val) {
+                curr.next = curr.next.next;
+
+            } else {
+                curr = curr.next;
+            }
+            
+        }
+        return dummyHead.next;
+
     }
 }
