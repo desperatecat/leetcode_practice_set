bd02d5cd69a5edc274ea5bcf31fa21699579d391
desperatecat <376198935@qq.com>
2020-02-18 11:56:56 -0500

    s

--- Top_200+_Freq_by_Tag/LinkedList/easy/237. Delete Node in a Linked List.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/237. Delete Node in a Linked List.java
@@ -1,3 +1,5 @@
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC237%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E5%88%A0%E9%99%A4%E9%93%BE%E8%A1%A8%E4%B8%AD%E7%9A%84%E8%8A%82%E7%82%B9.md
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,6 +10,7 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        
+        node.val = node.next.val;
+        node.next = node.next.next;
     }
 }
