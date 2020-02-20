0811b3c2d559fb8a0d4383de7f2f1b426a947079
desperatecat <376198935@qq.com>
2020-02-20 10:15:00 -0500

    add cmt

--- Top_200+_Freq_by_Tag/HashTable/easy/219. Contains Duplicate II.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/219. Contains Duplicate II.java
@@ -1,9 +1,11 @@
+//https://leetcode.com/problems/contains-duplicate-ii/discuss/61572/Java-solution-using-HashMap's-put()
+
 public class Solution {
 public boolean containsNearbyDuplicate(int[] nums, int k) {
     Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 	
 	for(int i = 0; i <  nums.length; i++) {
-		Integer ord = map.put(nums[i], i); //if repeat return the repeat value(the key's value),else return null
+		Integer ord = map.put(nums[i], i); //if repeat return the repeat value(the key's value, which is previous i),else return null
 		if(ord != null && i - ord <= k) {
 			return true;
 		}
