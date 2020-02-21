a5afd3b4ff1715b29146bef567c473e54348f885
desperatecat <376198935@qq.com>
2020-02-21 13:39:02 -0500

    290 s

---
+++ Top_200+_Freq_by_Tag/HashTable/easy/290. Word Pattern.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/word-pattern/discuss/73402/8-lines-simple-Java
+
+class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.split(" ");
+        if (words.length != pattern.length())
+            return false;
+        Map index = new HashMap();
+        for (Integer i=0; i<words.length; ++i)
+            if (index.put(pattern.charAt(i), i) != index.put(words[i], i))
+                return false;
+        return true;
+    }
+}
