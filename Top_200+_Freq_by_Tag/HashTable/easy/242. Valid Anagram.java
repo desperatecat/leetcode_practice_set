17733ecb6d8214130fafe8f4ac5c54cf1cceae9e
desperatecat <376198935@qq.com>
2020-02-21 13:27:54 -0500

    242 s1, s2

---
+++ Top_200+_Freq_by_Tag/HashTable/easy/242. Valid Anagram.java
@@ -0,0 +1,37 @@
+//sorting
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+        return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+}
+
+
+
+//hash table
+//https://mp.weixin.qq.com/s/o5HTxmOgpftSaQdebS9zyQ
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+        return false;
+        }
+        int[] table = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            table[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < t.length(); i++) {
+            table[t.charAt(i) - 'a']--;
+            //if table[t.charAt(i) - 'a'] < 0, t contains a letter which not exist in s
+            if (table[t.charAt(i) - 'a'] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
