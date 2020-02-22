7ca76559906dd584316481db16ba99e09d7aea01
desperatecat <376198935@qq.com>
2020-02-22 00:17:16 -0500

    swap

--- Top_200+_Freq_by_Tag/HashTable/easy/387. First Unique Character in a String.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/387. First Unique Character in a String.java
@@ -1,18 +1,3 @@
-//https://leetcode.com/problems/first-unique-character-in-a-string/discuss/86348/Java-7-lines-solution-29ms
-
-class Solution {
-    public int firstUniqChar(String s) {
-        int freq [] = new int[26];
-        for(int i = 0; i < s.length(); i ++)
-            freq [s.charAt(i) - 'a'] ++;
-        for(int i = 0; i < s.length(); i ++)
-            if(freq [s.charAt(i) - 'a'] == 1)
-                return i;
-        return -1;
-    }
-}
-
-
 //hashmap
 class Solution {
     public int firstUniqChar(String s) {
@@ -29,6 +14,21 @@ class Solution {
             if (count.get(s.charAt(i)) == 1) 
                 return i;
         }
+        return -1;
+    }
+}
+
+
+
+//https://leetcode.com/problems/first-unique-character-in-a-string/discuss/86348/Java-7-lines-solution-29ms
+class Solution {
+    public int firstUniqChar(String s) {
+        int freq [] = new int[26];
+        for(int i = 0; i < s.length(); i ++)
+            freq [s.charAt(i) - 'a'] ++;
+        for(int i = 0; i < s.length(); i ++)
+            if(freq [s.charAt(i) - 'a'] == 1)
+                return i;
         return -1;
     }
 }
