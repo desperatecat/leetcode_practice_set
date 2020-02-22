5e3beab7cdf155587b27471a1f60c32e51496027
desperatecat <376198935@qq.com>
2020-02-22 00:31:47 -0500

    lint fix

--- Top_200+_Freq_by_Tag/HashTable/easy/463. Island Perimeter.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/463. Island Perimeter.java
@@ -10,13 +10,13 @@ class Solution {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (grid[i][j] == 1) {
-                    islands++; // count islands
-                    if (i < grid.length - 1 && grid[i + 1][j] == 1) neighbours++; // count down neighbours
-                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) neighbours++; // count right neighbours
+                    island++; // count islands
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) neighbour++; // count down neighbours
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) neighbour++; // count right neighbours
                 }
             }
         }
 
-        return islands * 4 - neighbours * 2;
+        return island * 4 - neighbour * 2;
     }
 }
