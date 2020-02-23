f6f7d82365809570e17da0e205a2c97b67ee0701
desperatecat <376198935@qq.com>
2020-02-23 01:44:13 -0500

    add cmt2

--- Top_200+_Freq_by_Tag/Stack/easy/20. Valid Parentheses.java
+++ Top_200+_Freq_by_Tag/Stack/easy/20. Valid Parentheses.java
@@ -13,7 +13,7 @@ class Solution {
             
             //suppose ']' is the first element of s, then the stack will be empty, stack.isEmpty() is true, which will make s as invalid; 
             //suppose s is valid, the stack should contains a lot of ']' or '}' or ')', when all the left brackets are gone througth, c will jump to the right brackets, then this if will check whether c is relative right bracket or not, if not, return false
-            else if (stack.isEmpty() || stack.pop() != c)   //stack.pop() will actually pop the top element, no matter the if statement is true or false
+            else if (stack.isEmpty() || stack.pop() != c)   //stack.pop() will actually pop up the top element, no matter the if statement is true or false; stack.peek() will return the top element of a stack, but not pop out any element
                 return false;
 	    }
 	    return stack.isEmpty(); //The stack must be empty if the parentheses is valid. If not, such as "((", the stack will still hold two ')' chars. The stack is not empty.
