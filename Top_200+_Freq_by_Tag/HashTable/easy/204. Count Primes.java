31442526e1d16ea17dda05c0700184a6432dd9a2
desperatecat <376198935@qq.com>
2020-02-23 14:26:05 -0500

    204 cmt

--- Top_200+_Freq_by_Tag/HashTable/easy/204. Count Primes.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/204. Count Primes.java
@@ -1,13 +1,16 @@
 //https://github.com/grandyang/leetcode/issues/204
+//Sieve of Eratosthenes
+
+
 class Solution {
     public int countPrimes(int n) {
-         boolean[] notPrime = new boolean[n];
+        boolean[] notPrime = new boolean[n];    //boolean default value is false
         int count = 0;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n; i++) { //it starts from 2, the first prime, then mark the multip of 2 as true in notPrime, so the loop of i will skip them. the next prime is 3, do the same thing. Then it is 4, which 2*2, so the not prime is true, and will skip to next.
             if (notPrime[i] == false) {
                 count++;
                 for (int j = 2; i*j < n; j++) {
-                    notPrime[i*j] = true;
+                    notPrime[i*j] = true;   
                 }
             }
         }
