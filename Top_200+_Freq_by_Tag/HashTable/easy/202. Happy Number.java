8ffe1aabe79602bfbe2826ac36791f814cde1ffe
desperatecat <376198935@qq.com>
2020-02-23 13:46:08 -0500

    202 cmt

--- Top_200+_Freq_by_Tag/HashTable/easy/202. Happy Number.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/202. Happy Number.java
@@ -3,7 +3,7 @@ class Solution {
         Set<Integer> seen = new HashSet<>();
         while(n!=1 && !seen.contains(n)){
             seen.add(n);
-            n=getNext(n);
+            n=getNext(n);   //the sum will be a new n, and going to next getNext invoke
         }
         return n==1;
     }
@@ -12,9 +12,9 @@ class Solution {
     private int getNext(int n){
         int totalSum = 0;
         while(n>0){
-            int d = n%10;
-            n = n/10;
-            totalSum += d*d;
+            int d = n%10;   //first iter will be digit, next iter will be tens digit
+            n = n/10;  
+            totalSum += d*d;    
         }
         return totalSum;
     }
