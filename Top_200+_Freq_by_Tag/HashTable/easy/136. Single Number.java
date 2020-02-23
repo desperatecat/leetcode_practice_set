5f0dcd8fac3651410055fa0909969dae25c5b513
desperatecat <376198935@qq.com>
2020-02-23 13:31:03 -0500

    136 s2

--- Top_200+_Freq_by_Tag/HashTable/easy/136. Single Number.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/136. Single Number.java
@@ -1,11 +1,27 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int x = 0;
-       for (int i =0; i < nums.length ; i++){
+        for (int i =0; i < nums.length ; i++){
             x ^= nums[i];   //a⊕0=a, a⊕a=0, a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
-       } 
+        } 
         
         return x;
+    
+    }
+}
+
+
+
+//Hash Set
+class Solution {
+    public int singleNumber(int[] nums) {
+        Set<Integer> s = new HashSet<Integer>();
+        for(int i:nums){    //i represents nums[i] in this case
+            if(!s.add(i)){  //if s already contains i, !s.add(i) will return true, then remove i 
+                 s.remove(i);
+            }
+        }
+        return s.iterator().next(); //return the one that's not been removed
     
     }
 }
