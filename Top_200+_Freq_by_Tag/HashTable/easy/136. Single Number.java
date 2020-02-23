681aefd1f5f9f48ef90b7b5e92628bd82e32c1b8
desperatecat <376198935@qq.com>
2020-02-23 13:21:03 -0500

    136 cmt

--- Top_200+_Freq_by_Tag/HashTable/easy/136. Single Number.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/136. Single Number.java
@@ -2,7 +2,7 @@ class Solution {
     public int singleNumber(int[] nums) {
         int x = 0;
        for (int i =0; i < nums.length ; i++){
-            x ^= nums[i];
+            x ^= nums[i];   //a⊕0=a, a⊕a=0, a⊕b⊕a=(a⊕a)⊕b=0⊕b=b
        } 
         
         return x;
