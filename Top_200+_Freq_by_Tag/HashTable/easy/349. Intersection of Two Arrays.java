7fa548b144b200fb21d426230e13f0b02d613948
desperatecat <376198935@qq.com>
2020-02-23 20:30:55 -0500

    redo

--- Top_200+_Freq_by_Tag/HashTable/easy/349. Intersection of Two Arrays.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/349. Intersection of Two Arrays.java
@@ -1,16 +1,16 @@
 class Solution {
   public int[] intersection(int[] nums1, int[] nums2) {
     HashSet<Integer> set1 = new HashSet<Integer>();
-    for (Integer n : nums1) set1.add(n);
+    for (Integer n : nums1) set1.add(n);  //set contains object
     HashSet<Integer> set2 = new HashSet<Integer>();
     for (Integer n : nums2) set2.add(n);
 
     set1.retainAll(set2);  //retain all set2 element in set1, and remove other elements which are not in set2
 
     int [] output = new int[set1.size()];
-    int idx = 0;
+    int i = 0;
     for (int s : set1) {
-        output[idx++] = s;
+        output[i++] = s;
     }
     return output;
   }
--- Top_200+_Freq_by_Tag/HashTable/easy/350. Intersection of Two Arrays II.java
+++ Top_200+_Freq_by_Tag/HashTable/easy/350. Intersection of Two Arrays II.java
@@ -3,23 +3,30 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap();
 
         // freq count for nums1
-        for (int num : nums1) map.put(num, map.getOrDefault(num, 0) + 1);   //map.put(num, map.getOrDefault(num, 0) + 1) is the same as: if (map.containsKey(num)) map.put(num, map.get(num) + 1); else map.put(num, 1);
-        
+        for (int n1 : nums1) {
+            map.put(n1, map.getOrDefault(n1, 0) + 1);   //map.put(n1, map.getOrDefault(n1, 0) + 1) is the same as: if (map.containsKey(n1)) map.put(n1, map.get(n1) + 1); else map.put(n1, 1);
+        }
         // collect result
         ArrayList<Integer> result = new ArrayList();
         
-        for (int num: nums2) {
-            if (map.containsKey(num)){
-                result.add(num);
-                map.put(num, map.get(num) - 1); //if nums2 contains the same element as nums1, freq - 1
-                map.remove(num, 0);
+        for (int n2: nums2) {
+            if (map.containsKey(n2)){
+                result.add(n2);
+                map.put(n2, map.get(n2) - 1); //if n2 contains the same element as n1, freq - 1
+                map.remove(n2, 0); //if freq is 0, remove the pair (n2--0) from the map
             }
         }
        // convert result
-       int[] r = new int[result.size()];
-       for(int i = 0; i < result.size(); i++) {
-           r[i] = result.get(i);
+       int[] output = new int[result.size()];
+        int i = 0;
+       for(int r:result) {
+           output[i++] = r;
        }
-       return r;
+       return output;
     }
 }
+
+//why we need to map.remove(n2, 0)?
+// for example, input:[4,9,5], [9,4,9,8,4]
+// if we don't remove, output will be [9,4,9,4] instead of [4,9],
+// because the key will still exist while nums2 is iterating (but now nums1 didn't contain this key any more)
