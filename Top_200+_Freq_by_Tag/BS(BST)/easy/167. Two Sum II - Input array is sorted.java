4a11f1f6ab9c09f25029168bd64d355ef40fb9da
desperatecat <376198935@qq.com>
2020-02-25 19:31:40 -0500

    167 s1

---
+++ Top_200+_Freq_by_Tag/BS(BST)/easy/167. Two Sum II - Input array is sorted.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<numbers.length; i++){
+            map.put(numbers[i], i);
+        }
+        
+        for(int i=0; i<numbers.length; i++){
+            int temp = target - numbers[i];
+            if(map.containsKey(temp) && map.get(temp)!=i){
+                if(map.get(temp) > i){
+                    return new int[]{i+1, map.get(temp)+1};
+                }
+                return new int[]{map.get(temp)+1, i+1};
+            }
+        }
+        throw new IllegalArgumentException("No two sum result!");
+    }
+}
+
+
+
