2cd9e5fa4579f8c1b76d7a915f33dcab74b4b7dc
desperatecat <376198935@qq.com>
2020-02-25 19:32:01 -0500

    167 s2

--- Top_200+_Freq_by_Tag/BS(BST)/easy/167. Two Sum II - Input array is sorted.java
+++ Top_200+_Freq_by_Tag/BS(BST)/easy/167. Two Sum II - Input array is sorted.java
@@ -1,3 +1,4 @@
+//array
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -19,4 +20,28 @@ class Solution {
 }
 
 
-
+//bs
+//https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/discuss/51239/Share-my-java-AC-solution.
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] indice = new int[2];
+        if(numbers == null || numbers.length<2) return indice;
+        int left = 0, right = numbers.length-1;
+        
+        while(left<right){
+            int v = numbers[left]+ numbers[right];
+            if(v == target){
+                indice[0] = left+1;
+                indice[1] = right+1;
+                break;
+            } else if (v>target){
+                right--;
+            } else {
+                left++;
+            }
+            
+        }
+        return indice;
+        
+    }
+}
