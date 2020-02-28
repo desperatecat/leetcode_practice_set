0940273de46c222fac534931193e50d0e3bb22b2
desperatecat <376198935@qq.com>
2020-02-28 14:09:30 -0500

    add cmt

--- Top_200+_Freq_by_Tag/Two Pointers/easy/88. Merge Sorted Array.java
+++ Top_200+_Freq_by_Tag/Two Pointers/easy/88. Merge Sorted Array.java
@@ -15,3 +15,16 @@ class Solution {
     }
     
 }
+
+
+//nums1[finished--] = (nums1[tail1] > nums2[tail2]) ? nums1[tail1--] : nums2[tail2--];
+// nums1 = [1,2,3,0,0,0], m = 3, tail1 = 2
+// nums2 = [2,5,6],       n = 3, tail2 = 2
+// finished = 5
+
+// nums1[5] = nums2[2] == 6, tail2 = 1, finished = 4
+// nums1[4] = nums2[1] == 5, tail2 = 0, finished = 3
+// nums1[3] = nums1[2] == 3, tail1 = 1, finished = 2
+// nums1[2] = nums2[0] == 2, tail1 = 0, finished = 1, tail2 = -1
+// nums1[1] = nums1[0] == 2, tail1 = -1, finished = 0
+// nums1[0] = nums1[0], jump out from while, stay the same
