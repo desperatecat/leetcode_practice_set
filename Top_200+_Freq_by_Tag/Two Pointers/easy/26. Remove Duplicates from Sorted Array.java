3970b3df141a58d10257c59e89ff1988b6ff5e46
desperatecat <376198935@qq.com>
2020-02-27 21:53:41 -0500

    add cmt

--- Top_200+_Freq_by_Tag/Two Pointers/easy/26. Remove Duplicates from Sorted Array.java
+++ Top_200+_Freq_by_Tag/Two Pointers/easy/26. Remove Duplicates from Sorted Array.java
@@ -1,3 +1,6 @@
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC26%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E5%88%A0%E9%99%A4%E6%8E%92%E5%BA%8F%E6%95%B0%E7%BB%84%E4%B8%AD%E7%9A%84%E9%87%8D%E5%A4%8D%E9%A1%B9.md
+//two pointers
+
 class Solution {
     public int removeDuplicates(int[] nums) {
         if(nums.length == 0) return 0;
