9b3dd86256e53f37255c44ede3eb72ae79afc508
desperatecat <376198935@qq.com>
2020-02-29 19:04:33 -0500

    add cmt

--- Top_200+_Freq_by_Tag/LinkedList/easy/141. Linked List Cycle.java
+++ Top_200+_Freq_by_Tag/LinkedList/easy/141. Linked List Cycle.java
@@ -13,7 +13,7 @@
 
 
 //https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC141%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E7%8E%AF%E5%BD%A2%E9%93%BE%E8%A1%A8.md
-
+//two pointers
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head, slow = head;
@@ -24,6 +24,24 @@ public class Solution {
 
             if(fast == slow) return true;
 
+        }
+        return false;
+    }
+}
+
+
+
+//hash table
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodesSeen = new HashSet<>();
+        while (head != null) {
+            if (nodesSeen.contains(head)) {
+                return true;
+            } else {
+                nodesSeen.add(head);
+            }
+            head = head.next;
         }
         return false;
     }
