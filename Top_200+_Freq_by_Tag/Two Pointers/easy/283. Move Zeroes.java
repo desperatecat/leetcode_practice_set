82e80387f3b717fa5f3ea30f5c900f70d786c5f1
desperatecat <376198935@qq.com>
2020-03-01 19:39:43 -0500

    s2

--- Top_200+_Freq_by_Tag/Two Pointers/easy/283. Move Zeroes.java
+++ Top_200+_Freq_by_Tag/Two Pointers/easy/283. Move Zeroes.java
@@ -12,3 +12,21 @@ class Solution {
         }
     }
 }
+
+
+
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC283%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E7%A7%BB%E5%8A%A8%E9%9B%B6.md
+//swap
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+                j++;
+            }
+        }
+    }
+}
