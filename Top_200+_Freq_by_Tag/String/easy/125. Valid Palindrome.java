b44c1d813d6249d768bcd0b330322d713e3d74ef
desperatecat <376198935@qq.com>
2020-03-05 20:09:12 -0500

    125

---
+++ Top_200+_Freq_by_Tag/String/easy/125. Valid Palindrome.java
@@ -0,0 +1,22 @@
+//https://github.com/MisterBooo/LeetCodeAnimation/blob/master/notes/LeetCode%E7%AC%AC125%E5%8F%B7%E9%97%AE%E9%A2%98%EF%BC%9A%E9%AA%8C%E8%AF%81%E5%9B%9E%E6%96%87%E4%B8%B2.md
+class Solution {
+    public boolean isPalindrome(String s) {
+        if(s.length() == 0)
+             return true;
+        int l = 0, r = s.length() - 1;
+        while(l < r){
+            //make sure the charcter is letter or digit
+            if(!Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+            }else if(!Character.isLetterOrDigit(s.charAt(r))){
+                r--;
+            }else{
+                if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+                    return false;
+                l++;
+                r--;
+            } 
+        }
+        return true;
+    }
+}
