e5c7e27ff821b0d9bfdce20d1201d8880359ac69
desperatecat <376198935@qq.com>
2020-03-12 21:22:58 -0400

    s2

--- Top_200+_Freq_by_Tag/Array/medium/238. Product of Array Except Self.java
+++ Top_200+_Freq_by_Tag/Array/medium/238. Product of Array Except Self.java
@@ -3,14 +3,19 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] res = new int[n];
-        res[0] = 1;
-        for (int i = 1; i < n; i++) {
-            res[i] = res[i - 1] * nums[i - 1];
+        // Calculate lefts and store in res.
+        int left = 1;
+        for (int i = 0; i < n; i++) {
+            if (i > 0)
+                left = left * nums[i - 1];
+            res[i] = left;
         }
+        // Calculate rights and the product from the end of the array.
         int right = 1;
         for (int i = n - 1; i >= 0; i--) {
+            if (i < n - 1)
+                right = right * nums[i + 1];
             res[i] *= right;
-            right *= nums[i];
         }
         return res;
     }
