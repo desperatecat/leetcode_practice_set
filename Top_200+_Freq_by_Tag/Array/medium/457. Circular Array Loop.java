4a6c13157b8378d874c5d29c52f334336ccdf437
desperatecat <376198935@qq.com>
2020-03-14 20:44:35 -0400

    457 a

---
+++ Top_200+_Freq_by_Tag/Array/medium/457. Circular Array Loop.java
@@ -0,0 +1,36 @@
+//https://github.com/grandyang/leetcode/issues/457
+//https://leetcode.com/problems/circular-array-loop/discuss/94187/Java-solution-easy-to-follow
+class Solution {
+    public boolean circularArrayLoop(int[] nums) {
+        boolean found = false;
+
+        for ( int n=0; n<nums.length; n++ ) {
+            Integer ps = n;
+            Integer pf = next(nums, 0, n);
+            int dir = nums[n];
+
+            while ( ps != null && pf != null && ps != pf ) {
+                ps = next(nums, dir, ps);
+                pf = next(nums, dir, next(nums, dir, pf));
+            }
+
+            if ( ps != null && ps == pf ) {
+                found = true;
+                break;
+            }
+        }
+
+        return found;
+    }
+
+    Integer next(int[] nums, int dir, Integer pos) {
+        if ( pos == null ) return null; // null, return null
+        if ( dir * nums[pos] < 0 ) return null; // change in direction, return null
+
+        Integer next = (pos + nums[pos]) % nums.length;
+        if ( next < 0 ) next += nums.length; // wrap negative
+
+        if ( next == pos ) next = null; // self-pointer, return null
+        return next;
+    }
+}
