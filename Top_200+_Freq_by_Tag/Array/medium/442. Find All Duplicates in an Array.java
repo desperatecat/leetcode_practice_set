5ccc32a406867bb3171933c7768fb3ff79c3f5a0
desperatecat <376198935@qq.com>
2020-03-14 20:39:42 -0400

    442 s

---
+++ Top_200+_Freq_by_Tag/Array/medium/442. Find All Duplicates in an Array.java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/discuss/92387/Java-Simple-Solution
+class Solution {
+    // when find a number i, flip the number at position i-1 to negative. 
+    // if the number at position i-1 is already negative, i is the number that occurs twice.
+    
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < nums.length; ++i) {
+            int index = Math.abs(nums[i])-1;
+            if (nums[index] < 0)
+                res.add(Math.abs(index+1));
+            nums[index] = -nums[index];
+        }
+        return res;
+    }
+}
