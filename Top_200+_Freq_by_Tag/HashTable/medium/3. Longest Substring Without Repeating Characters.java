ffe16bf228fbe4af087bd844accc7038ce23706f
desperatecat <376198935@qq.com>
2020-03-18 19:49:39 -0400

    3 s2

--- Top_200+_Freq_by_Tag/HashTable/medium/3. Longest Substring Without Repeating Characters.java
+++ Top_200+_Freq_by_Tag/HashTable/medium/3. Longest Substring Without Repeating Characters.java
@@ -17,3 +17,24 @@ class Solution {
         return ans;
     }
 }
+
+
+
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/discuss/1729/11-line-simple-Java-solution-O(n)-with-explanation
+//hashmap & two pointers
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max=0;
+        for (int i=0, j=0; i<s.length(); ++i){
+            if (map.containsKey(s.charAt(i))){
+                j = Math.max(j,map.get(s.charAt(i))+1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-j+1);
+        }
+        return max;
+    
+    }
+}
