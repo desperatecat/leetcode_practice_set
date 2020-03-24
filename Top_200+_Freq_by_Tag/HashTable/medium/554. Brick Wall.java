e184a4769f6451875c1a23ca963471a9f7c31dbb
desperatecat <376198935@qq.com>
2020-03-23 20:59:22 -0400

    554

---
+++ Top_200+_Freq_by_Tag/HashTable/medium/554. Brick Wall.java
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/brick-wall/discuss/101728/I-DON'T-THINK-THERE-IS-A-BETTER-PERSON-THAN-ME-TO-ANSWER-THIS-QUESTION
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        if(wall.size() == 0) return 0;
+        int count = 0;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(List<Integer> list : wall){
+            int length = 0;
+            for(int i = 0; i < list.size() - 1; i++){
+                length += list.get(i);
+                map.put(length, map.getOrDefault(length, 0) + 1);
+                count = Math.max(count, map.get(length));
+            }
+        }
+        return wall.size() - count;
+    }
+}
