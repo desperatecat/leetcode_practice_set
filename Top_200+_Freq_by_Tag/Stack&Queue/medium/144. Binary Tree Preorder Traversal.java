1ec4cecac8f34bb8ef6c454b37264f4cbc4bbbd5
desperatecat <376198935@qq.com>
2020-03-23 21:01:19 -0400

    144

---
+++ Top_200+_Freq_by_Tag/Stack&Queue/medium/144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,32 @@
+//https://leetcode.com/problems/binary-tree-preorder-traversal/discuss/45266/Accepted-iterative-solution-in-Java-using-stack.
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode node) {
+        List<Integer> list = new LinkedList<Integer>();
+        Stack<TreeNode> rights = new Stack<TreeNode>();
+        while(node != null) {
+            list.add(node.val);
+            if (node.right != null) {
+                rights.push(node.right);
+            }
+            node = node.left;
+            if (node == null && !rights.isEmpty()) {
+                node = rights.pop();
+            }
+        }
+        return list;
+    }
+}
+
+
