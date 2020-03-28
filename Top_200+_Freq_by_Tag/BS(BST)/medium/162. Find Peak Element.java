ef8dfc8eadd57a7612a0c57e58b60e26a8d178a5
desperatecat <376198935@qq.com>
2020-03-27 21:55:59 -0400

    162

---
+++ Top_200+_Freq_by_Tag/BS(BST)/medium/162. Find Peak Element.java
@@ -0,0 +1,12 @@
+//https://github.com/grandyang/leetcode/issues/162
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return right;
+    }
+}
