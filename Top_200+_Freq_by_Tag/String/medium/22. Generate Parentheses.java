dc7570fe1f8e692619933931f3052154d8ff8f9f
desperatecat <376198935@qq.com>
2020-04-02 20:02:49 -0400

    22

---
+++ Top_200+_Freq_by_Tag/String/medium/22. Generate Parentheses.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/generate-parentheses/discuss/10100/Easy-to-understand-Java-backtracking-solution
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+    
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+        
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
+}
