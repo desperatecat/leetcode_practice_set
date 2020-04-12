1db6d4067bfcb6bbc61cb0b96dfa3b034e0f15b4
desperatecat <376198935@qq.com>
2020-04-12 18:53:41 -0400

    53 s2

--- Top_200+_Freq_by_Tag/Divided and Conquer/easy/53. Maximum Subarray.java
+++ Top_200+_Freq_by_Tag/Divided and Conquer/easy/53. Maximum Subarray.java
@@ -37,3 +37,27 @@ class Solution {
       return leftMax + rightMax;
     }
 }
+
+
+
+//
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==0){return 0;}
+        
+        int sum=nums[0];
+        int max=nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            if(sum<0){
+                sum=nums[i];
+            }
+            else sum+=nums[i];
+            
+            if(sum>max){
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
