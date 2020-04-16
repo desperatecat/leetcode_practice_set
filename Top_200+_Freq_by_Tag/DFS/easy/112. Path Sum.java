15d30b72807615bb84cdee6fde98eb839c2c233e
desperatecat <376198935@qq.com>
2020-04-15 20:20:03 -0400

    112

---
+++ Top_200+_Freq_by_Tag/DFS/easy/112. Path Sum.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+//https://leetcode.com/problems/path-sum/discuss/36378/AcceptedMy-recursive-solution-in-Java
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null) return false;
+    
+        if(root.left == null && root.right == null && sum - root.val == 0) return true;
+    
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
