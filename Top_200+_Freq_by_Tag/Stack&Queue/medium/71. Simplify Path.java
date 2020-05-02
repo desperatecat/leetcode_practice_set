c2e184115cb958c9c5aeb071043ed5f88712f569
desperatecat <376198935@qq.com>
2020-05-02 14:50:33 -0400

    71

---
+++ Top_200+_Freq_by_Tag/Stack&Queue/medium/71. Simplify Path.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/simplify-path/discuss/25686/Java-10-lines-solution-with-stack
+class Solution {
+    public String simplifyPath(String path) {
+        Deque<String> stack = new LinkedList<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..",".",""));
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) stack.pop();
+            else if (!skip.contains(dir)) stack.push(dir);
+        }
+        String res = "";
+        for (String dir : stack) res = "/" + dir + res;
+        return res.isEmpty() ? "/" : res;
+    }
+}
