cdeac8952ba065ee9edb4919ff39c640a49bf4d7
desperatecat <376198935@qq.com>
2020-05-07 22:29:19 -0400

    103

--- Top_200+_Freq_by_Tag/Stack&Queue/medium/103. Binary Tree Zigzag Level Order Traversal.java
+++ Top_200+_Freq_by_Tag/Stack&Queue/medium/103. Binary Tree Zigzag Level Order Traversal.java
@@ -13,6 +13,8 @@
  *     }
  * }
  */
+
+
 //https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/33904/JAVA-Double-Stack-Solution
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
@@ -44,5 +46,36 @@ class Solution {
             if(!tmp.isEmpty()) ans.add(tmp);
        }
        return ans;
+    }
+}
+
+
+
+
+//https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/discuss/33815/My-accepted-JAVA-solution
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) 
+    {
+        List<List<Integer>> sol = new ArrayList<>();
+        travel(root, sol, 0);
+        return sol;
+    }
+    
+    private void travel(TreeNode curr, List<List<Integer>> sol, int level)
+    {
+        if(curr == null) return;
+        
+        if(sol.size() <= level)
+        {
+            List<Integer> newLevel = new LinkedList<>();
+            sol.add(newLevel);
+        }
+        
+        List<Integer> collection  = sol.get(level);
+        if(level % 2 == 0) collection.add(curr.val);
+        else collection.add(0, curr.val);
+        
+        travel(curr.left, sol, level + 1);
+        travel(curr.right, sol, level + 1);
     }
 }
