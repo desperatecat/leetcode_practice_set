d983b3611f40a0cf21fa8f56b63207676da1602b
desperatecat <376198935@qq.com>
2020-05-07 22:27:08 -0400

    93 s2

--- Top_200+_Freq_by_Tag/Backtracking/medium/93. Restore IP Addresses.java
+++ Top_200+_Freq_by_Tag/Backtracking/medium/93. Restore IP Addresses.java
@@ -21,3 +21,38 @@ class Solution {
         return true;
     }
 }
+
+
+
+//backtracking
+//https://leetcode.com/problems/restore-ip-addresses/discuss/31095/Backtracking-solution-in-Java-easy-to-understand
+public class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> result = new ArrayList<String>();
+        if(s == null || s.length() == 0 || s.length() > 12){
+            return result;
+        }
+        StringBuilder builder = new StringBuilder();
+        helper(result, s, builder, 0, 0);
+        return result;
+    }
+    
+    private void helper(List<String> result, String s, StringBuilder builder, int start, int count){
+        if(start == s.length() && count == 3){
+            result.add(builder.toString());
+            return;
+        }
+        for(int i = start + 1; i <= s.length(); i++){
+            String tmp = s.substring(start, i);
+            if(tmp.length() > 3 || tmp.length() > 1 && tmp.charAt(0) == '0' || Integer.parseInt(tmp) > 255){
+                return;
+            }
+            StringBuilder newBuilder = new StringBuilder(builder);
+            if(newBuilder.length() != 0){
+                newBuilder.append(".");
+            }
+            newBuilder.append(tmp);
+            helper(result, s, newBuilder, i, newBuilder.length() == tmp.length() ? count : count + 1);
+        }
+    }
+}
