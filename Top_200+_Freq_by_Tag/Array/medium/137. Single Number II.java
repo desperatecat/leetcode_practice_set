43100f1bec0f8f3ec6e4be5584496cc2114cdede
desperatecat <376198935@qq.com>
2020-05-12 17:57:05 -0400

    137

---
+++ Top_200+_Freq_by_Tag/Array/medium/137. Single Number II.java
@@ -0,0 +1,21 @@
+
+//https://leetcode.com/problems/single-number-ii/discuss/43297/Java-O(n)-easy-to-understand-solution-easily-extended-to-any-times-of-occurance
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            for(int j = 0; j < nums.length; j++) {
+                if(((nums[j] >> i) & 1) == 1) {
+                    sum++;
+                    sum %= 3;
+                }
+            }
+            if(sum != 0) {
+                ans |= sum << i;
+            }
+        }
+        return ans;
+    }
+}
+
