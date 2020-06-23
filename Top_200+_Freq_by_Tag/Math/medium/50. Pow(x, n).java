ddaff9ce2bf040dd2abcdc33c3f5313cd9dc6465
desperatecat <376198935@qq.com>
2020-06-23 19:35:52 -0400

    50

---
+++ Top_200+_Freq_by_Tag/Math/medium/50. Pow(x, n).java
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/powx-n/discuss/19546/Short-and-easy-to-understand-solution
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(n == Integer.MIN_VALUE){
+            x = x * x;
+            n = n/2;
+        }
+        if(n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+
+        return (n%2 == 0) ? myPow(x * x, n/2) : x *  myPow(x * x, n/2);
+    }
+}
