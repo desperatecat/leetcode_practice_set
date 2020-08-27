3b7407b199d48102dec25ac7269a2c5f67c18e9a
desperatecat <376198935@qq.com>
2020-08-27 08:43:48 +0800

    309

---
+++ Top_300+_Freq_by_Tag/DP/medium/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -0,0 +1,14 @@
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75927/Share-my-thinking-process
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int sell = 0, prev_sell = 0, buy = Integer.MIN_VALUE, prev_buy;
+        for (int price : prices) {
+            prev_buy = buy;
+            buy = Math.max(prev_sell - price, prev_buy);
+            prev_sell = sell;
+            sell = Math.max(prev_buy + price, prev_sell);
+        }
+        return sell;
+    }
+}
