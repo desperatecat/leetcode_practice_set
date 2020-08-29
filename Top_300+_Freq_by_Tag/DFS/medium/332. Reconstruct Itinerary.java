028dad4ec5acf2acd32a587c808988d61e944780
desperatecat <376198935@qq.com>
2020-08-29 13:14:04 +0800

    332

---
+++ Top_300+_Freq_by_Tag/DFS/medium/332. Reconstruct Itinerary.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/reconstruct-itinerary/discuss/78768/Short-Ruby-Python-Java-C%2B%2B
+class Solution {
+    public List<String> findItinerary(String[][] tickets) {
+        for (String[] ticket : tickets)
+            targets.computeIfAbsent(ticket[0], k -> new PriorityQueue()).add(ticket[1]);
+        visit("JFK");
+        return route;
+    }
+
+    Map<String, PriorityQueue<String>> targets = new HashMap<>();
+    List<String> route = new LinkedList();
+
+    void visit(String airport) {
+        while(targets.containsKey(airport) && !targets.get(airport).isEmpty())
+            visit(targets.get(airport).poll());
+        route.add(0, airport);
+    }
+}
